9b132f86eda569fa04305cc726020f0d88c3183d
Washington Luis <washingtonluismm@gmail.com>
2022-01-20 00:38:13 -0800

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
 
+
 function Directory(props){
     
     const renderDirectoryItem = ({item}) => {
@@ -10,11 +11,12 @@ function Directory(props){
             <ListItem
                 title={item.name}
                 subtitle={item.description}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/react-lake.jpg')}}
             />
         );
     }
-    
+
     return (
         <FlatList
             data={props.campsites}
