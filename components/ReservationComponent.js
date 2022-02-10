c867b4df91b61c0b455b2075ffeb997b3cdbf404
Washington Luis <washingtonluismm@gmail.com>
2022-02-09 20:15:19 -0800

    WS w3 2-3

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
-    Picker, Switch, Button, Modal } from 'react-native';
+    Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
 
@@ -14,7 +14,6 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
         };
     }
 
@@ -22,13 +21,28 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
-    }
-
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+        var date = this.state.date.getDate();
+        var month = this.state.date.getMonth() + 1;
+        var year = this.state.date.getFullYear();
+   
+        Alert.alert(
+            'Begin Search?',
+            'Number of Campers: ' + this.state.campers + '\n' + 'Hike-In? ' + this.state.hikeIn + '\n' + 'Date: ' + month + '/' + date + '/' + year,
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                    onPress: () => console.log('Cancel Pressed')
+                },
+                {
+                    text: 'OK',
+                    onPress: () => console.log("OK Pressed")
+                }
+            ],
+            { cancelable: false }
+        );
     }
 
     resetForm() {
@@ -48,8 +62,6 @@ class Reservation extends Component {
                     animation='zoomIn'
                     duration={2000}
                     delay={1000}
-                    // ref={view}
-                    // {...panResponder.panHandlers}
                 >  
                     <View style={styles.formRow}>
                         <Text style={styles.formLabel}>Number of Campers</Text>
@@ -105,33 +117,6 @@ class Reservation extends Component {
                             accessibilityLabel='Tap me to search for available campsites to reserve'
                         />
                     </View>
-                    <Modal
-                        animationType={'slide'}
-                        transparent={false}
-                        visible={this.state.showModal}
-                        onRequestClose={() => this.toggleModal()}
-                    >
-                        <View style={styles.modal}>
-                            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                            <Text style={styles.modalText}>
-                                Number of Campers: {this.state.campers}
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}
-                            </Text>
-                            <Text style={styles.modalText}>
-                                Date: {this.state.date.toLocaleDateString('en-US')}
-                            </Text>
-                            <Button
-                                onPress={() => {
-                                    this.toggleModal();
-                                    this.resetForm();
-                                }}
-                                color='#5637DD'
-                                title='Close'
-                            />
-                        </View>
-                    </Modal>
                 </Animatable.View>
             </ScrollView>
         );
@@ -152,22 +137,6 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
-    },
-    modal: { 
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
     }
 });
 
