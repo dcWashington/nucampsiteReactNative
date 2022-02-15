9a7beda6e83368628a5ede533fc243cd2d59e7ea
Washington Luis <washingtonluismm@gmail.com>
2022-02-14 18:55:27 -0800

    WS w4 1-3

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 class LoginTab extends Component {
 
@@ -159,9 +160,15 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(imgUri, [{resize: {width: 400}}], {format: 'png' });
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
     }
 
     handleRegister() {
